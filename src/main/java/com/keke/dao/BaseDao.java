0685d731337f7c8d7ec17e6cd1b51b362c99f3dd
zhouxiaolong1 <zhouxiaolong@xiangrikui.com>
2015-06-21 23:05:42 +0800

    rrrs

---
+++ src/main/java/com/keke/dao/BaseDao.java
@@ -0,0 +1,16 @@
+/*
+ * @(#) BaseDao.java
+ * @Author:keke(keke_erp@163.com) 2015年6月18日
+ * @Copyright (c) 2015 Keke Limited. All rights reserved.
+ */
+package com.keke.dao;
+
+/**
+  * @author keke (keke_erp@163.com) 2015年6月18日
+  * @version 1.0
+  * @Function 类功能说明
+  */
+public class BaseDao {
+
+}
+
